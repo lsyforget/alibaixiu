c0bf064e48f7116df86dc229956144f23dc06a5e
syli <594813687@qq.com>
2020-02-08 20:24:26 +0800

    搭建项目

---
+++ routes/actions/other/loginStatus.js
@@ -0,0 +1,8 @@
+module.exports = async (req, res) => {
+	if (req.session && req.session.userInfo) {
+		// res.send('var isLogin = true;var userId=123;')
+		res.send(`var isLogin = true;var userId="${req.session.userInfo._id}";`)
+	}else {
+		res.send('var isLogin = false')
+	}
+};
